31e9ab889b2badce53b8c03659f0e183655e8c41
Katta sai swaroop <swaroopsai927@gmail.com>
2026-06-23 18:39:08 +0530

    Add files via upload

---
+++ utils/search.py
@@ -0,0 +1,17 @@
+from tavily import TavilyClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = TavilyClient(
+    api_key=os.getenv("TAVILY_API_KEY")
+)
+
+def web_search(query):
+    result = client.search(
+        query=query,
+        search_depth="advanced",
+        max_results=5
+    )
+    return str(result)
