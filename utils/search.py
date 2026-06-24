371c2cd27e4e1b78f2c5a59c6f4b0bb68dbf4acb
Katta sai swaroop <swaroopsai927@gmail.com>
2026-06-24 10:06:53 +0530

    Update search.py

--- utils/search.py
+++ utils/search.py
@@ -1,17 +1,31 @@
 from tavily import TavilyClient
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
-client = TavilyClient(
-    api_key=os.getenv("TAVILY_API_KEY")
-)
+# Local (.env) OR Streamlit Cloud (Secrets)
+api_key = os.getenv("TAVILY_API_KEY")
+
+if not api_key:
+    api_key = st.secrets["TAVILY_API_KEY"]
+
+client = TavilyClient(api_key=api_key)
+
 
 def web_search(query):
-    result = client.search(
-        query=query,
-        search_depth="advanced",
-        max_results=5
-    )
-    return str(result)
+
+    try:
+
+        result = client.search(
+            query=query,
+            search_depth="basic",
+            max_results=5
+        )
+
+        return str(result)
+
+    except Exception as e:
+
+        return f"❌ Search Error: {str(e)}"
